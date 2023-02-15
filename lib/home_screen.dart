d48250692e8d0291809c6d972b8eb4ce6bad25ba
Jahongir0203 <you@example.com>
2023-02-15 19:30:19 +0300

    chore(flutter): initial commit

---
+++ lib/home_screen.dart
@@ -0,0 +1,282 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  List Categories = [
+    'UI/UX',
+    'Coding',
+    'App Design',
+    'Basics',
+  ];
+
+  List Courses = ['Web Design', 'App Development', 'UI Design', 'Ecommerce'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.white,
+      child: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.only(top: 25.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 18.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Choose your",
+                            style: TextStyle(
+                              fontSize: 20.0,
+                              color: Colors.black54,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 2,
+                          ),
+                          Text(
+                            "Desire Course",
+                            style: TextStyle(
+                              fontSize: 25.0,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          )
+                        ],
+                      ),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(15.0),
+                        child: Image(
+                          image: AssetImage('images/profile.jpg'),
+                          height: 50.0,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 20, vertical: 25),
+                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                  decoration: BoxDecoration(
+                    color: Color(0xFFF8FAFB),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Searche for course',
+                      border: InputBorder.none,
+                      suffixIcon: Icon(Icons.search),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 15),
+                  child: Text(
+                    'Category',
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                SizedBox(
+                  height: 35,
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      shrinkWrap: true,
+                      itemCount: 4,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          margin: EdgeInsets.only(left: 20),
+                          padding: EdgeInsets.symmetric(horizontal: 25),
+                          decoration: BoxDecoration(
+                            color: index == 0 ? Colors.blueAccent : null,
+                            border: Border.all(color: Colors.blue.shade300),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Center(
+                            child: Text(
+                              Categories[index],
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w500,
+                                color: index == 0 ? Colors.white : Colors.blue,
+                              ),
+                            ),
+                          ),
+                        );
+                      }),
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                SizedBox(
+                  height: 110,
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      shrinkWrap: true,
+                      itemCount: 4,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.only(left: 20),
+                          child: Stack(
+                            children: [Container(
+                              margin: EdgeInsets.only(left: 20),
+                              padding: EdgeInsets.only(left: 100,right: 10,bottom: 15,top: 10),
+                              decoration: BoxDecoration(
+                                color: Color(0xFFF8FAFB),
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    Courses[index],
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w500,
+                                      color: Colors.blue,
+                                    ),
+                                  ),
+
+                                  Row(
+                                    children: [
+                                      Text(
+                                        '24 lessons',
+                                        style: TextStyle(
+                                          color: Colors.black54,
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        width: 5,
+                                      ),
+                                      Text(
+                                        '4.3',
+                                        style: TextStyle(color: Colors.black54),
+                                      ),
+                                      Icon(Icons.star, color: Colors.blue,
+                                      size: 15,),
+                                    ],
+                                  ),
+
+                                  Row(
+                                    children: [
+                                      Text(
+                                        '\$25',
+                                        style: TextStyle(
+                                          color: Colors.blue,
+                                        ),
+                                      ),
+                                      SizedBox(width: 40,),
+                                      InkWell(
+                                        onTap: (){},
+                                        child: Container(
+                                          padding: EdgeInsets.all(3),
+                                          decoration: BoxDecoration(
+                                            color: Colors.blue,
+                                            borderRadius:BorderRadius.circular(5),
+                                          ),
+                                          child: Icon(Icons.add,
+                                          color: Colors.white,
+                                          size: 20,),
+                                        ),
+
+                                      )
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ),
+                            Center(
+                              child: Padding(
+                                padding: EdgeInsets.only(right: 40),
+                                child:ClipRRect(
+                                  borderRadius: BorderRadius.circular(20),
+                                  child:Image(image: AssetImage('images/${Courses[index]}.png',),
+                                  height: 80,),
+                                ),
+                              ),
+                            )
+                            ]
+                          ),
+                        );
+                      }),
+                ),
+                SizedBox(
+                  height:30,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 15),
+                  child: Text(
+                    'Popular courses !',
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                GridView.builder(
+                  shrinkWrap: true,
+                  itemCount: Courses.length,
+                  physics: NeverScrollableScrollPhysics(),
+                  gridDelegate:SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+                itemBuilder: (context,index){
+                  return InkWell(
+                    onTap: (){},
+                    child: Container(
+                      margin: EdgeInsets.symmetric(horizontal: 15,vertical: 10),
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Color(0xFFF8FAFB),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [Text(Courses[index],
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black,
+                        ),),
+                          SizedBox(height: 10,),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [Text('24 lessons',
+                            style: TextStyle(
+                              color: Colors.black54,
+                            ),),
+
+                              Row(
+                                children: [
+                                  Text('4.3',
+                                    style: TextStyle(
+                                      color: Colors.black54,
+                                    ),),
+                                  Icon(Icons.star,
+                                  size: 18,
+                                  color: Colors.blue,),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
